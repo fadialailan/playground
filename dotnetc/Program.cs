04b37ca466dcfc02f186aacb3a16e4beeac9ff1a
Fadi Alailan <fadialailan@protonmail.com>
2024-04-14 10:58:51 +0300

    added editorconfig and made the Program use dotnet 5 style

--- dotnetc/Program.cs
+++ dotnetc/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
